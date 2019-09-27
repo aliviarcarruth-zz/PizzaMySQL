9f8737a312990096e666489e4d74fcfb6b91814b
Alivia <aliviarochester@gmail.com>
2019-09-27 14:18:00 -0400

    add form verification

--- add.php
+++ add.php
@@ -1,25 +1,51 @@
 <?php
 
+    $title = $email = $ingredients = '';
+
+    $errors = array(
+            'email' => '',
+            'title' => '',
+            'ingredients' => ''
+    );
+
             if(isset($_POST['submit'])) {
 
                 //check email
                 if (empty($_POST['email'])) {
-                    echo 'An email is required <br />';
+                    $errors['email'] = 'An email is required <br />';
                 } else {
-                    echo htmlspecialchars($_POST['email']);
+                    $email = $_POST['email'];
+                    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                        $errors['email'] = 'Email must be a valid email address';
+                    }
                 }
 
                 if (empty($_POST['title'])) {
-                    echo 'A title is required <br />';
+                    $errors['title'] = 'A title is required <br />';
                 } else {
-                    echo htmlspecialchars($_POST['title']);
+                    $title = $_POST['title'];
+                    if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+                       $errors['title'] ='Title must be letters and spaces only';
+                    }
 
                 }
-                if (empty($_POST['email'])) {
-                    echo 'At least one ingredient is required <br />';
+                if (empty($_POST['ingredients'])) {
+                    $errors['ingredients'] = 'At least one ingredient is required <br />';
                 } else {
-                    echo htmlspecialchars($_POST['ingredients']);
+                    $ingredients = $_POST['ingredients'];
+                    if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+                        $errors['ingredients'] ='Title must be letters and spaces only';
+                    }
                 }
+
+                if(array_filter($errors)){
+//                    echo 'errors in the form';
+                }   else {
+//                    echo 'form is valid';
+                    header('Location: index.php');
+                }
+
+
             } //end of POST check
 ?>
 
@@ -33,11 +59,14 @@ include ('templates/header.php');?>
     <h4 class="center">Add a Pizza</h4>
     <form action="add.php" class="white" method="POST">
         <label for="">Your Email:</label>
-        <input type="text" name="email">
+        <input type="text" name="email" value="<?php echo htmlspecialchars($email);  ?>">
+        <div class="red-text"> <?php echo $errors['email'];?> </div>
         <label for="">Pizza Title</label>
-        <input type="text" name="title">
+        <input type="text" name="title"  value="<?php echo htmlspecialchars($title); ?>">
+        <div class="red-text"> <?php echo $errors['title'];?> </div>
         <label for="">Ingredients (comma separated):</label>
-        <input type="text" name="ingredients">
+        <input type="text" name="ingredients"  value="<?php echo htmlspecialchars($ingredients); ?>">
+        <div class="red-text"> <?php echo $errors['ingredients'];?> </div>
         <div class="center">
             <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
