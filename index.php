16e19699ab344050c121b65e32c842ae58d1426a
Alivia <aliviarochester@gmail.com>
2019-10-03 12:35:35 -0400

    add delete option for db

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ include('templates/header.php');
                 <?php endforeach; ?>
                 </ul>
                 <div class="card-action right-align">
-                    <a href="#" class="brand-text">more info</a>
+                    <a href="details.php?id=<?php echo $pizza['id'] ?>" class="brand-text">more info</a>
                 </div>
             </div>
         </div>
